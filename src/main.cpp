6288b0c3faafdccc873146731406fbed7de4652a
Benjamin Anderson <bda.talos@gmail.com>
2022-05-09 10:33:13 -0500

    init

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <avr/io.h>
+
+void setup()
+{
+  PORTA.DIR = PIN4_bm;
+}
+
+void loop()
+{
+
+  PORTA.OUT = 00001000;
+
+  delay(1000);
+
+  PORTA.OUT = 00000000;
+
+  delay(1000);
+}
+
+// https://github.com/bda-talos/Hot-Plate.git
